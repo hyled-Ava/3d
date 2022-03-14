db7c4219a2e74c9e6d0699f3bf5a8d3bbd939b00
meteor <a1971242708@126.com>
2022-03-14 18:27:40 +0800

    标题栏设计好

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="less">
+body {
+  background: url("./assets/bg3.png") no-repeat top center;
+  background-size: 100% 100%;
+}
+
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+      <h1>天威机房态势感知平台</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+header {
+  height: 1.25rem;
+  width: 100%;
+  background: url("../assets/bt3.png") no-repeat top center;
+
+  h1 {
+    font-size: 0.375rem;
+    color: #fff;
+    text-align: center;
+    line-height: 1rem;
+  }
+}
+
+</style>
